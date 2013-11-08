31aeb51a9283a0f8109f65afbbe49f554d499d31
Kevin <admin@learnopengles.com>
2013-11-08 09:27:38 -0500

    Add support for the touch events on iOS and set the preferred frame rate to 60fps.

--- src/platform/ios/ViewController.m
+++ src/platform/ios/ViewController.m
@@ -25,6 +25,8 @@
     GLKView *view = (GLKView *)self.view;
     view.context = self.context;
     view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    view.userInteractionEnabled = YES;
+    self.preferredFramesPerSecond = 60;
     
     [self setupGL];
 }
@@ -41,6 +43,41 @@
     [EAGLContext setCurrentContext:self.context];
     on_surface_created();
     on_surface_changed([[self view] bounds].size.width, [[self view] bounds].size.height);
+}
+
+static CGPoint getNormalizedPoint(UIView* view, CGPoint locationInView)
+{
+    const float normalizedX = (locationInView.x / view.bounds.size.width) * 2.f - 1.f;
+    const float normalizedY = -((locationInView.y / view.bounds.size.height) * 2.f - 1.f);
+    return CGPointMake(normalizedX, normalizedY);
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    UITouch* touchEvent = [touches anyObject];
+    CGPoint locationInView = [touchEvent locationInView:self.view];
+    CGPoint normalizedPoint = getNormalizedPoint(self.view, locationInView);
+    on_touch_press(normalizedPoint.x, normalizedPoint.y);
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesMoved:touches withEvent:event];
+    UITouch* touchEvent = [touches anyObject];
+    CGPoint locationInView = [touchEvent locationInView:self.view];
+    CGPoint normalizedPoint = getNormalizedPoint(self.view, locationInView);
+    on_touch_drag(normalizedPoint.x, normalizedPoint.y);
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesEnded:touches withEvent:event];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [super touchesCancelled:touches withEvent:event];
 }
 
 #pragma mark - GLKView and GLKViewController delegate methods
